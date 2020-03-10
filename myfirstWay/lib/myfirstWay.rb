e77115c60751ddd3b9d430b4691cf9944d968775
Xavier Perez <xperez007@gmail.com>
2020-03-10 17:36:30 +0000

    Initial commit

---
+++ myfirstWay/lib/myfirstWay.rb
@@ -0,0 +1,6 @@
+require "myfirstWay/version"
+#testing
+module MyfirstWay
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ myfirstWay/lib/myfirstWay/version.rb
@@ -0,0 +1,3 @@
+module MyfirstWay
+  VERSION = "0.1.0"
+end
